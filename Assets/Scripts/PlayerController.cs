799179cfde87f04bb864aa459d81a4d5c3225577
Shiigetaka <gracjanwojciechowski4@gmail.com>
2025-06-18 20:04:28 +0200

    project to repo

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private int myNumber = 13;
+    void Start()
+    {
+        myNumber = 14;
+    }
+
+    void Update()
+    {
+        Debug.Log("yoo");
+    }
+}
