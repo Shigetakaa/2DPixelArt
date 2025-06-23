15eee2dbd5967377d7e73b1fe9d137ff0e67752e
Shiigetaka <gracjanwojciechowski4@gmail.com>
2025-06-23 22:39:53 +0200

    Dodanie przeglądania menu głównego oraz panel pauzy

---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Przycisk rozpocznij
+    public void OnStartGamePress()
+    {
+
+    }
+
+    // Przycisk ulepszenia
+    public void OnUpgradesPress()
+    {
+
+    }
+
+    // Przycisk instrukcja
+    public void OnInstructionsPress()
+    {
+
+    }
+
+    // Przycisk ustawienia
+    public void OnSettingsPress()
+    {
+
+    }
+
+    // Przycisk wyjdź
+    public void OnCloseGamePress()
+    {
+        
+    }
+}
