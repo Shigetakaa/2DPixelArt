4f1dc071079970bdb42fb55ed4eb4473dcb87af9
Shiigetaka <gracjanwojciechowski4@gmail.com>
2025-06-24 19:22:07 +0200

    Zrobienie menu pauzy, możliwość wyjścia z gry, rozpoczęcia rozgrywki

--- Assets/Scripts/UI/InGameUIManager.cs
+++ Assets/Scripts/UI/InGameUIManager.cs
@@ -1,22 +1,28 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class InGameUIManager : MonoBehaviour
 {
+    public GameObject pauseMenu;
+
     // Przycisk wróć do gry
     public void OnResumePress()
     {
-        
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
     }
 
     // Przycisk menu główne
     public void OnMenuPress()
     {
-        
+        SceneManager.LoadScene("MainMenu");
     }
 
     // Przycisk wyjdź
     public void OnCloseGamePress()
     {
-
+        Application.Quit();
     }
 }
