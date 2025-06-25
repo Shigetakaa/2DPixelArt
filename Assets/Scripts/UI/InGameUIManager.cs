54639dfb912db277ac09a8ec3c9319517c43942b
Shiigetaka <gracjanwojciechowski4@gmail.com>
2025-06-25 18:27:00 +0200

    zrobienie game over screena oraz healthbar

--- Assets/Scripts/UI/InGameUIManager.cs
+++ Assets/Scripts/UI/InGameUIManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class InGameUIManager : MonoBehaviour
 {
     public GameObject pauseMenu;
+    public GameObject gameOverScreen;
+    public GameObject healthBar;
 
     // Przycisk wróć do gry
     public void OnResumePress()
@@ -24,5 +26,16 @@ public class InGameUIManager : MonoBehaviour
     public void OnCloseGamePress()
     {
         Application.Quit();
+    }
+
+    // UI Końca gry
+    public void GameOverScreen()
+    {
+        gameOverScreen.SetActive(true);
+        // Wyłączenie paska zdrowia po śmierci gracza
+        if (healthBar != null)
+        {
+            healthBar.SetActive(false);
+        }
     }
 }
