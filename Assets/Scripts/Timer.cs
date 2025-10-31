fdacca35ea594c6a8f839434ff86f45438fa4705
Shiigetaka <gracjanwojciechowski4@gmail.com>
2025-10-31 15:27:29 +0100

    zrobienie i ustawienie timera

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public float remainingTime;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Odliczanie czasu w dół
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+            timerText.color = Color.red;
+        }
+
+        // Rozdzielenie czasu na minuty i sekundy
+        int min = Mathf.FloorToInt(remainingTime / 60);
+        int sec = Mathf.FloorToInt(remainingTime % 60);
+
+        // Ustawienie formatu tekstu
+        timerText.text = string.Format("{0:00}:{1:00}", min, sec);
+    }
+}
