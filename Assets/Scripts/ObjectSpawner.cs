74000a4e0c66bb54907cf083af681606c2d6f7e1
Shiigetaka <gracjanwojciechowski4@gmail.com>
2026-02-01 20:06:04 +0100

    dodanie wody

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -1,15 +1,20 @@
 using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class ObjectSpawner : MonoBehaviour
 {
     public GameObject[] elementsMeadow;
     public GameObject[] elementsDesert;
+    public GameObject[] water;
 
-    public int elementsNumber = 200;
+    public int elementsNumber = 20000;
+    public int waterNumber = 50;
 
     public Vector2 elementsMinPos = new Vector2(-209f, -151f);
     public Vector2 elementsMaxPos = new Vector2(233f, 142f);
+    public Vector2 waterMinPos = new Vector2(-207f, -146f);
+    public Vector2 waterMaxPos = new Vector2(229f, 139f);
 
     public GameSettingsManager settings;
 
@@ -19,9 +24,25 @@ public class ObjectSpawner : MonoBehaviour
         settings = FindAnyObjectByType<GameSettingsManager>();
 
         ElementsSpawner();
+        WaterSpawner();
     }
 
-    private void ElementsSpawner()
+    public void WaterSpawner()
+    {
+        for(int i = 0; i < waterNumber; i++)
+        {
+            Vector2 waterPos = new Vector2(
+                UnityEngine.Random.Range(waterMinPos.x, waterMaxPos.x),
+                UnityEngine.Random.Range(waterMinPos.y, waterMaxPos.y)
+            );
+
+            int randomSpawn = UnityEngine.Random.Range(0, water.Length);
+            GameObject chosenWater = water[randomSpawn];
+            Instantiate(chosenWater, new Vector3(waterPos.x, waterPos.y, -1), Quaternion.identity);
+        }
+    }
+
+    public void ElementsSpawner()
     {
         for(int i = 0; i < elementsNumber; i++)
         {
